b269967c4b44a45f185704e0cb00ab26a62e55ed
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-10 12:40:34 +0700

    finished deleted message

--- index.js
+++ index.js
@@ -12,65 +12,72 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB Connetion Successfull");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/room/",roomRoutes)
-
+app.use("/api/room/", roomRoutes);
 
 const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+    console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    credentials: true,
-  },
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
 });
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
-  
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    // console.log("Co nguoi online");
-    onlineUsers.set(userId, socket.id);
-    // console.log("online: "+onlineUsers);
-    // console.log(onlineUsers);
-  });
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        // console.log("Co nguoi online");
+        onlineUsers.set(userId, socket.id);
+        // console.log("online: "+onlineUsers);
+        // console.log(onlineUsers);
+    });
 
-  socket.on("send-msg", (data) => {
-    console.log(data.to.length);
-    const sendUserSocket = onlineUsers.get(data.to);
-    // console.log(sendUserSocket);
-    if (sendUserSocket) {
-      console.log(data);
-      socket.to(sendUserSocket).emit("msg-recieve", {msg:data.msg,react:data.react,id:data.id,namesend:data.namesend,avatarImage:data.avatarImage});
-  
-    }
-    if(data.to.length>2)
-    {
-      for(let i = 0 ; i < data.to.length;i++)
-      {
-        const sendUserSocket = onlineUsers.get(data.to[i]);
-    // console.log(sendUserSocket);
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        // console.log("Sender socket " + sendUserSocket);
         if (sendUserSocket) {
-          console.log(data);
-          socket.to(sendUserSocket).emit("msg-recieve", {msg:data.msg,react:data.react,id:data.id,namesend:data.namesend,avatarImage:data.avatarImage});
-      
+            socket.to(sendUserSocket).emit("msg-recieve", {
+                id: data.id,
+                msg: data.msg,
+                react: data.react,
+                namesend: data.namesend,
+                avatarImage: data.avatarImage,
+                files: data.files,
+                image: data.image,
+            });
         }
-      }
-    }
-  });
-  
+
+        if (data.to.length > 2) {
+            for (let i = 0; i < data.to.length; i++) {
+                const sendUserSocket = onlineUsers.get(data.to[i]);
+
+                if (sendUserSocket) {
+                    socket.to(sendUserSocket).emit("msg-recieve", {
+                        image: data.image,
+                        id: data.id,
+                        msg: data.msg,
+                        react: data.react,
+                        namesend: data.namesend,
+                        avatarImage: data.avatarImage,
+                        files: data.files,
+                    });
+                }
+            }
+        }
+    });
 });
